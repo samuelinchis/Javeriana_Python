cdafe2e5ec2d1975c70977b5d9937a599e4be3a5
Samuel Rueda Sánchez <110745715+samuelinchis@users.noreply.github.com>
2022-08-10 13:06:51 -0500

    programa para operaciones
    se hace suma resta multiplicación y potencia mediante funciones

---
+++ 0x05-Functions.py
@@ -0,0 +1,16 @@
+def addition (sumando1, sumando2):
+    suma = sumando1 + sumando2
+    print ("resultado de la suma:", suma)
+addition (3,8)
+def substraction (minuendo, sustraendo):
+    diferencia = minuendo - sustraendo
+    print ("Resultado de la resta:", diferencia)
+substraction (5,11)
+def multiplication (factor1, factor2):
+    producto = factor1 * factor2
+    print ("El resultado de la multiplicacion:", producto)
+multiplication(4,8)
+def empowerment (base, exponente):
+    potencia = base**exponente
+    print ("El resultado de la potencia es:", potencia)
+empowerment (5,2)
